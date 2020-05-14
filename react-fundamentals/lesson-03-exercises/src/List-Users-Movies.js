f9d235d6135c008a3931b82b07ccb2f8f6955a54
szega <gaborszekeres44@gmail.com>
2020-05-14 13:14:50 +0200

    03/1: pass data into component via props

---
+++ react-fundamentals/lesson-03-exercises/src/List-Users-Movies.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ListUsersMovies extends Component {
+    render () {
+        return (
+        <div> 
+        <h2>Favorite Movies</h2>
+		<ol>
+            {this.props.profiles.map( item => (
+            <li key={this.props.profiles.id}>{this.props.users[item.userID].name} favorite movie is {this.props.movies[item.favoriteMovieID].name}</li>
+            ) ) }
+        </ol>
+        </div> 
+        );
+    }
+}
+
+export default ListUsersMovies;
